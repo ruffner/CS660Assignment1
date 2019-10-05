b1ef88fd0f3ed868200423cd64d9f5ac83bdbcf7
Matt Ruffner <mattruffner7@gmail.com>
2019-10-05 11:53:55 -0400

    working epsGreedyAgent

--- bandits/epsGreedyAgent.py
+++ bandits/epsGreedyAgent.py
@@ -1,4 +1,4 @@
-
+import math
 import numpy as np
 import sys
 import copy
@@ -13,7 +13,23 @@ class epsGreedyAgent:
     def __init__(self):
         #self.currentState.print_board()
         self.name = "Eric the Epsilon Greedy Agent"
-    
+
+    def eps(self,t):
+        return 1-math.exp((-1*t)/200)
+
     def recommendArm(self, bandit, history):
-        #Hey, your code goes here!
-        return False
+        narms = bandit.getNumArms()
+        Nt = [0]*narms
+        Q = [0]*narms
+
+        if len(history):
+            for turn in history:
+                Nt[turn[0]] += (1 if turn[1] else 0)
+            Q = [n/len(history) for n in Nt]
+
+        ee = random.random()
+
+        if ee < self.eps(len(history)):
+            return Q.index(max(Q))
+        else:
+            return round( random.random()*(narms-1) )
